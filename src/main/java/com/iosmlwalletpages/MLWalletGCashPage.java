42801309c1983c42d8be2d729be8b26be5558486
FrancisMercado <mlqaautomation@mlhuillier.com>
2024-01-16 15:35:47 +0800

    gcash xpath

---
+++ src/main/java/com/iosmlwalletpages/MLWalletGCashPage.java
@@ -0,0 +1,38 @@
+package com.iosmlwalletpages;
+
+import org.openqa.selenium.By;
+
+public class MLWalletGCashPage {
+
+    public static By objGcash =By.xpath("//XCUIElementTypeOther[@name=\"GCash\"]");
+    public static By objSendMoneyBtn =By.xpath("public static By objGcash =By.xpath(\"//XCUIElementTypeOther[@name=\"GCash\"]\");");
+    public static By objGcashServicesLabel =By.xpath("//XCUIElementTypeStaticText[@name=\"GCash Services\"]\");");
+    public static By objBackBtn =By.xpath("//XCUIElementTypeOther[@name=\"\uE5CB\"]");
+    public static By objGCashNoTextbox =By.xpath("//XCUIElementTypeOther[@name=\"9** *** ****\"]/XCUIElementTypeTextField\n");
+    public static By getObjSendMoneyGCashLabel =By.xpath("//XCUIElementTypeStaticText[@name=\"Send Money to GCash\"]\n");
+    public static By objSelectfromContactsBtn =By.xpath("//XCUIElementTypeOther[@name=\"4UB1RX\"]");
+    public static By objGcashNextBtn =By.xpath("//XCUIElementTypeOther[@name=\"ZRU39Y\"]");
+    public static By objContactsBtn =By.xpath("//XCUIElementTypeOther[@name=\"Contacts\"])[2]");
+    public static By objFavoritesBtn =By.xpath("//XCUIElementTypeOther[@name=\"Favorites\"]");
+    public static By objSearchBox =By.xpath("//XCUIElementTypeTextField[@name=\"OO7ML4\"]\n");
+    public static By objContactsLabel =By.xpath("//XCUIElementTypeStaticText[@name=\"Contacts\"]\n");
+    public static By objSelectContacts =By.xpath("//XCUIElementTypeOther[@name=\"Firstname1 Lastname1 +56911122236 \uF06F\"]\n");
+    public static By objAddFavoriteContact =By.xpath("//XCUIElementTypeStaticText[@name=\"Contacts\"]\n");
+    public static By objGcashNextSendMoneyBtn  =By.xpath("//XCUIElementTypeOther[@name=\"O7QH8K\"]\n");
+    public static By objAmountSendLabel =By.xpath("//XCUIElementTypeStaticText[@name=\"Amount to Send\"]");
+    public static By objAmountSendTextbox =By.xpath("//XCUIElementTypeOther[@name=\"Amount to Send PHP\"]/XCUIElementTypeOther/XCUIElementTypeTextField\n");
+    public static By objComfirmDetailsLabel =By.xpath("//XCUIElementTypeStaticText[@name=\"Confirm Details\"]\n");
+    public static By objSendMoneytoGcashLabel =By.xpath("//XCUIElementTypeStaticText[@name=\"Send Money to GCash\"]\n");
+    public static By objReceiverGcashNoValue =By.xpath("//XCUIElementTypeStaticText[@name=\"Receiver GCash Mobile #\"])[2]");
+    public static By objSenderNameValue =By.xpath("//XCUIElementTypeStaticText[@name=\"Sender Name\"])[2]\n");
+    public static By objSenderMobileNoValue =By.xpath("//XCUIElementTypeStaticText[@name=\"Sender Mobile No.\"])[2]\n");
+    public static By objAmountValue =By.xpath("//XCUIElementTypeStaticText[@name=\"Amount\"])[2]\n");
+    public static By objSerciceFeeValue =By.xpath("//XCUIElementTypeStaticText[@name=\"Service Fee\"])[2]\n");
+    public static By objTotalValue =By.xpath("//XCUIElementTypeStaticText[@name=\"Total\"])[2]\n");
+    public static By objGcashConfirmBtn =By.xpath("//XCUIElementTypeOther[@name=\"BZ9QFY\"]\n");
+    public static By objNoGCashOtherOptionLabel =By.xpath("//XCUIElementTypeStaticText[@name=\"This number does not have a GCash-registered account. Please check the number and try again.\"]");
+    public static By objSendViaMLKwartaPadalaBtn =By.xpath("//XCUIElementTypeOther[@label=\"Send Money via ML Kwarta Padala\"]");
+    public static By objSendViaMLKwartaWalletBtn =By.xpath("//XCUIElementTypeOther[@label=\"Send Money to ML Wallet User\"]");
+    public static By objCloseBtn =By.xpath("//XCUIElementTypeOther[@name=\"Close\"]");
+
+}
